d7f43608a9b71ecab1a31d0052ab78bdead126a6
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-04 16:53:17 +0900

    백 슬래시의 이스케이프 문자 처리

---
+++ Bronze5_10171.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+// 백 슬래시의 이스케이프 문자 처리
+int main()
+{
+    cout << "\\    /\\ " << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|";
+
+    return 0;
+}
