57730955ae3a12c93c5c9e12d7a9236d2c04e780
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-04 17:28:41 +0900

    Default_+-*/%

---
+++ Bronze5_110869.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cin >> a;
+    cin >> b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b;
+
+    return 0;
+}
