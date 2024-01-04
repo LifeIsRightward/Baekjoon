8a33a1c698001716ecf5df91e3e03dde4ffd7dc3
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-04 17:41:19 +0900

    int로 풀지 long long으로 풀지, 문제조건을 잘 봐야함.

---
+++ Bronze5_11382.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+// int로 풀지 long long으로 풀지, 문제조건을 잘 봐야함.
+int main()
+{
+    long long a, b, c = 0;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+
+    return 0;
+}
