344e8c87999c2dccd0f41f4fc46e8033c7b79550
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-08 02:03:19 +0900

    defaultFile_Setting

---
+++ 미해결/Bronze5_10189.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cout << "#  # #### #### #  #"
+       << "\n";
+  cout << "#### # # # # # # #### # # # # # #"
+       << "\n";
+  cout << "# # ######## # #";
+  return 0;
+}
---
+++ 새싹/Bronze5_15962.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cout << "파이팅!!";
+  return 0;
+}
