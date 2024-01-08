c6799f69c44723981168b9d9f075cf8fec405547
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-09 01:22:23 +0900

    2024_01_09

---
+++ 새싹/Bronze5_11022.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int a, b, T = 0;
+
+  cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    cin >> a >> b;
+    cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << "\n";
+  }
+  return 0;
+}
