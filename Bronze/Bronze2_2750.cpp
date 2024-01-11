22463f78c30c41ba1986adbbe2291750a988fa1e
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-12 00:01:37 +0900

    key : value index mapping and fastIO

--- Bronze/Bronze2_2750.cpp
+++ Bronze/Bronze2_2750.cpp
@@ -7,21 +7,26 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  vector<long long> vec;
-  long long N = 0;
-  int temp = 0;
-
+  int N, temp;
   cin >> N;
-  for (long long i = 0; i < N; i++) {
+
+  vector<int> vec(10001, 0);
+  for (int i = 0; i < N; i++) {
     cin >> temp;
-    vec.push_back(temp);
+    vec[temp]++;
   }
 
-  sort(vec.begin(), vec.end());
+  // for (int i = 1; i <= N; i++) {
+  //   cout << i << " : " << vec[i] << "\n";
+  // }
 
-  for (auto a : vec) {
-    cout << a << "\n";
+  // cout << "-------" << endl;
+  for (int i = 1; i <= vec.size(); i++) {
+    if (vec[i] != 0) {
+      for (int j = 0; j < vec[i]; j++) {
+        cout << i << "\n";
+      }
+    }
   }
-
   return 0;
 }
