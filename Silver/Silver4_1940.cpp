dec9d5dfc8884ab633eed21655eb3f6a76b59375
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-15 17:05:21 +0900

    map header

---
+++ Silver/Silver4_1940.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int testcase = 0;
+  int temp = 0;
+  cin >> testcase;
+
+  map<int, int> mapparr;
+
+  for (int i = 0; i < testcase; i++) {
+    cin >> temp;
+    mapparr[temp]++;
+    // 특이하지만 이런 방법도 가능하다. key : temp인 원소가 없으므로
+    // 생성한뒤, 기본값(value) : 0 에서 1을 더해준다.
+  }
+
+  int M = 0;
+  cin >> M;
+
+  for (int i = 0; i < M; i++) {
+    cin >> temp;
+    if (mapparr[temp] != 0) {
+      cout << "1"
+           << "\n";
+    } else {
+      cout << "0"
+           << "\n";
+    }
+  }
+  return 0;
+}
