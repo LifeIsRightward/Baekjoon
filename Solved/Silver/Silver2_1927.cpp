cdb585b75eb1257a216a1f067f3878d86ba530a2
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-31 02:00:54 +0900

    use the priority_queue and struct cmp(abs), min heap

--- Solved/Silver/Silver2_1927.cpp
+++ Solved/Silver/Silver2_1927.cpp
@@ -1,15 +1,28 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <queue>
 #include <string>
 #include <vector>
 using namespace std;
 
+struct compare {
+    bool operator()(int a, int b) {
+        if (abs(a) == abs(b)) { // 절댓값이 같은 경우
+            return a > b;
+            // 오름차순 a > b -> 오름차순
+            // 내림차순 a < b -> 내림차순
+        } else {
+            return abs(a) > abs(b); // 절댓값 크기 오름차순
+        }
+    }
+};
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    priority_queue<int, vector<int>, greater<int>> pq;
+    priority_queue<int, vector<int>, compare> pq;
     int temp = 0;
     int N = 0;
     cin >> N;
