725a80aaa8af78493ae2bb21b0bf771a7b8aec97
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-01-31 01:54:28 +0900

    use the priority_queue and greater(cmp), min heap

---
+++ Solved/Silver/Silver2_1927.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int temp = 0;
+    int N = 0;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        if (temp == 0) {
+            if (pq.empty()) {
+                cout << "0"
+                     << "\n";
+            } else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(temp);
+        }
+    }
+    return 0;
+}
