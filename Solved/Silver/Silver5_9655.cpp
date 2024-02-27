f0f74055e05e7ae95b43ac167f7c9a666bda8328
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-02-27 18:19:18 +0900

    just thinking

---
+++ Solved/Silver/Silver5_9655.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n = 0;
+  cin >> n;
+
+  if (n % 2 == 0) {
+    cout << "CY";
+  } else {
+    cout << "SK";
+  }
+  return 0;
+}
