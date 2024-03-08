13b56c80069b5e503ca96eadcaafd0d65adeda4f
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-03-08 20:34:53 +0900

    BackTracking

---
+++ Solved/Silver/Silver2_15650.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
