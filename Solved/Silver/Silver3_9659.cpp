f96b7af84e011cb8d72f8683376f9bd18c69ff9e
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-08-13 19:44:31 +0900

    Implement

---
+++ Solved/Silver/Silver3_9659.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n = 0;
+
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << "CY";
+    } else {
+        cout << "SK";
+    }
+    return 0;
+}
+
+// a = int(input())
+// if a % 2 == 0:
+//     print('CY')
+// else:
+//     print('SK')
