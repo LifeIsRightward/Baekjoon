09049bf6da311ff7d26f69548cd7376e9cd56768
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-08-13 16:45:45 +0900

    DP

--- DefaultFile/DefaultFile.cpp
+++ DefaultFile/DefaultFile.cpp
@@ -4,9 +4,13 @@
 #include <vector>
 using namespace std;
 
+#define fastIo                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL);
+
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  fastIo;
 
   return 0;
 }
