78f89a91918a6a21383a2a388d8f4cd1aade0992
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-08-22 22:18:24 +0900

    1735 modified

--- Solved/Silver/Silver3_1735.cpp
+++ Solved/Silver/Silver3_1735.cpp
@@ -17,14 +17,20 @@ int getgcd = 0;
 int main() {
     fastIo;
 
+    // 첫 번째 분수 입력
     cin >> topa >> bottoma;
+    // 두 번째 분수 입력
     cin >> topb >> bottomb;
 
+    // 분수의 합을 위한 분모 구하기
     commonb = bottoma * bottomb;
+    // 분수의 합을 위한 분자 구하기
     commont = (topa * bottomb) + (topb * bottoma);
 
+    // 기약분수 제조를 위한 gcd 찾기
     getgcd = __gcd(commonb, commont);
 
+    // 기약분수로 만들면서, 출력하기
     cout << commont / getgcd << " " << commonb / getgcd;
     return 0;
 }
