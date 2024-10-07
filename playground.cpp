6cda10f95ff6579d949b8d7c3315dfd0d7c78a47
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-10-08 02:31:22 +0900

    Playground

--- playground.cpp
+++ playground.cpp
@@ -1,18 +1,42 @@
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 using namespace std;
 
+#define fastIo                                                                                                                                                 \
+    ios_base::sync_with_stdio(false);                                                                                                                          \
+    cin.tie(NULL);                                                                                                                                             \
+    cout.tie(NULL);
+
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+    fastIo;
 
-  string str;
-  getline(cin, str);
-  cout << str << "\n";
-  getline(cin, str);
-  cout << str << "\n";
+    int T;
+    cin >> T;
 
-  return 0;
+    for (int t = 1; t <= T; ++t) {
+        int N;
+        cin >> N;
+
+        set<int> seen;
+        int cnt = 0;
+        int crtnum = 0;
+
+        while (seen.size() < 10) {
+            cnt++;
+            crtnum = N * cnt;
+
+            int tmp = crtnum;
+            while (tmp > 0) {
+                seen.insert(tmp % 10);
+                tmp /= 10;
+            }
+        }
+
+        cout << "#" << t << " " << crtnum << endl;
+    }
+
+    return 0;
 }
