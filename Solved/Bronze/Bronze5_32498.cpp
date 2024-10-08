139aa217f119063c6c8e18ebac784de4a2a9eb67
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-10-08 14:37:52 +0900

    Implement

---
+++ Solved/Bronze/Bronze5_32498.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int t = 0;
+int tmp = 0;
+int total = 0;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    cin >> tmp;
+
+    if (tmp % 2 != 0) {
+      total++;
+    }
+  }
+  return 0;
+}
