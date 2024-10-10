4f88b12e6aa5c6c9a58f225e8d45023d757858fa
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-10-10 15:39:29 +0900

    SWEA 2046

---
+++ SWEA/2046.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define fastIo                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL);
+
+int t = 0;
+
+int main() {
+  fastIo;
+
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    cout << "#";
+  }
+
+  return 0;
+}
