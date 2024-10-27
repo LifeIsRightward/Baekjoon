085a4433ff92d5c2f4fa8802878225c9487623e2
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-10-28 03:12:46 +0900

    SWEA

---
+++ SWEA/D1/2027.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define fastIo                                                                                                                                                 \
+    ios_base::sync_with_stdio(false);                                                                                                                          \
+    cin.tie(NULL);                                                                                                                                             \
+    cout.tie(NULL);
+
+int main() {
+    fastIo;
+
+    cout << "#++++" << "\n";
+    cout << "+#+++" << "\n";
+    cout << "++#++" << "\n";
+    cout << "+++#+" << "\n";
+    cout << "++++#" << "\n";
+    return 0;
+}
