703f10811d03dab0527cfc6fd034768de7cb4b23
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-10-28 03:35:18 +0900

    SWEA D1

---
+++ SWEA/D1/1543.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define fastIo                                                                                                                                                 \
+    ios_base::sync_with_stdio(false);                                                                                                                          \
+    cin.tie(NULL);                                                                                                                                             \
+    cout.tie(NULL);
+
+int n = 0;
+
+int main() {
+    fastIo;
+
+    cin >> n;
+
+    for (int i = n; i >= 0; i--) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
