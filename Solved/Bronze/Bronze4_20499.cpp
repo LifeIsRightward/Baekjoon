063cfc1786c0302cf6e0343bb23cd1c3fc433ca8
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-10-30 23:56:18 +0900

    String split

--- Solved/Bronze/Bronze4_20499.cpp
+++ Solved/Bronze/Bronze4_20499.cpp
@@ -5,6 +5,8 @@ int main() {
     int k, d, a;
     char trash;
     cin >> k >> trash >> d >> trash >> a;
+
+    cout << k << " " << d << " " << a << "\n";
     if (k + a < d || d == 0)
         cout << "hasu";
     else
