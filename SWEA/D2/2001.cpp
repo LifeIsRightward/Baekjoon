9c67b39a952dec04986c01b4e9165add0b361a3b
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-11-02 01:12:01 +0900

    SWEA 2001 Not Done

--- SWEA/D2/2001.cpp
+++ SWEA/D2/2001.cpp
@@ -6,36 +6,50 @@
 
 using namespace std;
 
-#define fastIo                      \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(NULL);                    \
-  cout.tie(NULL);
-
-int t = 0;
-int n, m = 0;
+#define fastIo                                                                                                                                                 \
+    ios_base::sync_with_stdio(false);                                                                                                                          \
+    cin.tie(NULL);                                                                                                                                             \
+    cout.tie(NULL);
 
 int main() {
-  fastIo;
+    fastIo;
 
-  cin >> t;
+    int t = 0;
+    int n, m = 0;
+    int total = 0;
 
-  for (int i = 0; i < t; i++) {
-    n, m = 0;
+    cin >> t;
 
-    cin >> n >> m;
-    int arr[n][n];
+    for (int i = 0; i < t; i++) {
+        n, m, total = 0;
 
-    // 배열 입력 받기
-    for (int j = 0; j < n; j++) {
-      for (int k = 0; k < n; k++) {
-        cin >> arr[j][k];
-      }
+        cin >> n >> m;
+        int arr[n][n];
+
+        // 배열 입력 받기
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                cin >> arr[j][k];
+            }
+        }
+
+        vector<int> vec;
+
+        int xpos, ypos = 0;
+
+        for (int i = xpos; i < n - m; i++) {
+        }
+
+        // for (int j = 0; j < n - m; j++) {
+        //     for (int q = j; q < j + m; q++) {
+        //         for (int f = j; f < j + m; f++) {
+        //             total += arr[q][f];
+        //         }
+        //     }
+        //     vec.push_back(total);
+        // }
+        // cout << *max_element(vec.begin(), vec.end()) << "\n";
     }
 
-    vector<int> vec;
-    for (int j = 0; j < n; j += m) {
-    }
-  }
-
-  return 0;
+    return 0;
 }
