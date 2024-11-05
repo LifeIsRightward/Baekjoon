6c7b8bcb90fe78d6b8569d0c5759bbd6acbc07a5
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-11-05 14:43:21 +0900

    not Donw 20352.cpp

---
+++ Solved/Bronze/Bronze4_20352.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define fastIo                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL);
+
+double width = 0;
+
+#define double pi = 3.14;
+
+int main() {
+  fastIo;
+
+  return 0;
+}
