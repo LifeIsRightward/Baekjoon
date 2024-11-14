82082d538e77786a7bf588b0fecb027d393c0e90
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-11-14 16:39:39 +0900

    SWEA 1984 Done

--- SWEA/D2/1984.cpp
+++ SWEA/D2/1984.cpp
@@ -5,32 +5,35 @@ using namespace std;
 int testcase = 0;
 
 vector<int> vec;
-int avg = 0;
+double avg = 0;
 int tmp = 0;
 
 int main() {
+  cin >> testcase;
 
-    cin >> testcase;
-
-    for (int i = 0; i < testcase; i++) {
-        vec.clear();
-        avg = 0;
-        for (int j = 0; j < 10; j++) {
-            cin >> tmp;
-            vec.push_back(tmp);
-        }
-
-        vec.erase(remove(vec.begin(), vec.end(), *max_element(vec.begin(), vec.end())), vec.end());
-        vec.erase(remove(vec.begin(), vec.end(), *min_element(vec.begin(), vec.end())), vec.end());
-
-        for (auto a : vec) {
-            avg += a;
-            cout << a << " ";
-        }
-
-        cout << '\n';
-
-        cout << "#" << i + 1 << " " << round(avg / vec.size()) << "\n";
+  for (int i = 0; i < testcase; i++) {
+    vec.clear();
+    avg = 0;
+    for (int j = 0; j < 10; j++) {
+      cin >> tmp;
+      vec.push_back(tmp);
     }
-    return 0;
+
+    // 이게 값으로 전달해야 하는데, remove에서 바로 *이걸로 넘겨버리면
+    // 포인터 주소를 넘기는 거라서 사실
+    // 값으로 대치 시켜서 집어넣는게 맞다고 한다
+    int maxele = *max_element(vec.begin(), vec.end());
+    int minele = *min_element(vec.begin(), vec.end());
+
+    vec.erase(remove(vec.begin(), vec.end(), maxele), vec.end());
+    vec.erase(remove(vec.begin(), vec.end(), minele), vec.end());
+
+    for (auto a : vec) {
+      avg += a;
+      //   cout << a << " ";
+    }
+
+    cout << "#" << i + 1 << " " << round(avg / vec.size()) << "\n";
+  }
+  return 0;
 }
