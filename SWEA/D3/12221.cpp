188424757c2c4510dcf4341480ff7012b7778c2e
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-11-17 02:07:44 +0900

    SWEA 12221

---
+++ SWEA/D3/12221.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastIo                                                                                                                                                 \
+    ios_base::sync_with_stdio(false);                                                                                                                          \
+    cin.tie(NULL);                                                                                                                                             \
+    cout.tie(NULL);
+
+int testcase = 0;
+
+int main() {
+    fastIo;
+
+    cin >> testcase;
+
+    for (int i = 0; i < testcase; i++) {
+        pair<int, int> a;
+        cin >> a.first >> a.second;
+
+        cout << "#" << i + 1 << " ";
+        if (a.first >= 10 || a.second >= 10) {
+            cout << "-1" << "\n";
+        } else {
+            cout << a.first * a.second << "\n";
+        }
+    }
+
+    return 0;
+}
