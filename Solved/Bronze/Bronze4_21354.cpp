300761c2f9922e9caf7d2d1340f5aae19ae3fb58
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-11-22 00:18:20 +0900

    Implements

---
+++ Solved/Bronze/Bronze4_21354.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int apple, pear, axel, petra;
+int main() {
+    cin >> apple >> pear;
+    axel = apple * 7;
+    petra = pear * 13;
+    if (axel > petra)
+        cout << "Axel";
+    else if (axel < petra)
+        cout << "Petra";
+    else
+        cout << "lika";
+}
