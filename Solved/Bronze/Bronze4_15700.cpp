83580cf0a5b9be0c1749dbcbfe9b06586ff1a52d
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-12-08 16:41:27 +0900

    Implement

---
+++ Solved/Bronze/Bronze4_15700.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  // FAST IO
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  long long n, m = 0;
+
+  cin >> n >> m;
+
+  cout << (n * m) / 2 << "\n";
+  return 0;
+}
