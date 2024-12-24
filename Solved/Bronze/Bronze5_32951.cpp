541ad9b2209a84a31898ab340ed3ed4915c9bcb4
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-12-24 20:40:12 +0900

    Implements

---
+++ Solved/Bronze/Bronze5_32951.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n = 0;
+
+    cin >> n;
+
+    cout << n - 2024 << "\n";
+
+    return 0;
+}
