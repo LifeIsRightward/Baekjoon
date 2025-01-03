9adb6885e49dcc2a7c691ca3ebb60b7fee36caf7
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-01-03 22:51:33 +0900

    Implements

---
+++ Solved/Bronze/Bronze4_27332.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int a, b = 0;
+
+    cin >> a >> b;
+
+    if (a + (7 * b) > 30) {
+        cout << "0" << "\n";
+    } else {
+        cout << "1" << "\n";
+    }
+
+    return 0;
+}
