d64a81025fd72a5fe76c845f5979b2fc4f931252
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-01-11 21:47:48 +0900

    Implements

---
+++ Solved/Bronze/Bronze4_15610.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    double a = 0;
+
+    cin >> a;
+
+    double b = sqrt(a);
+
+    cout << fixed;
+    cout.precision(7);
+    cout << 4 * b << "\n";
+
+    return 0;
+}
