90c794ded7917dbe8d25f6bc01f561d8609901f5
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-01-31 23:39:41 +0900

    Implements

---
+++ Solved/Bronze/Bronze3_2441.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j < i) {
+                cout << " ";
+            } else {
+                cout << "*";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
